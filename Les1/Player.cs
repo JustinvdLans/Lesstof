82d3f920980ffe61e73243b182b4f71bc83315ab
JustinvdLans <42778559+JustinvdLans@users.noreply.github.com>
2020-02-28 09:56:41 +0100

    Add files via upload

---
+++ Les1/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5f;
+
+    float hp = 100;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0f, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+
+        if (gameObject.tag == "Player")
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                hp -= 10;
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), "Health: " + hp);
+    }
+}
